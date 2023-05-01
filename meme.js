8149651ef7353a7e6d20f70e343f62ffd4816959
sisieb <130303366+sisieb@users.noreply.github.com>
2023-05-01 19:00:35 +0200

    Add files via upload

---
+++ meme.js
@@ -0,0 +1,32 @@
+const { EmbedBuilder,SlashCommandBuilder } = require('discord.js');
+ 
+module.exports = {
+ data: new SlashCommandBuilder()
+.setName('meme')
+.setDescription('This is a meme command!'),
+    async execute(interaction) {
+ 
+        async function meme() {
+            await fetch(`https://www.reddit.com/r/memes/random/.json`)
+            .then(async r => {
+ 
+                let meme = await r.json();
+ 
+                let title = meme[0].data.children[0].data.title;
+                let image = meme[0].data.children[0].data.url;
+                let author = meme[0].data.children[0].data.author;
+ 
+                const embed = new EmbedBuilder()
+                .setColor("Red")
+                .setTitle(`${title}`)
+                .setImage(`${image}`)
+                .setURL(`${image}`)
+                .setFooter({ text: author})
+ 
+                await interaction.reply({ embeds: [embed] });
+            })
+        }
+ 
+        meme();
+    }
+}
